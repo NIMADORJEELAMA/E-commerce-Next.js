a6c7e79c4d0a3fbbad5c5e395678525e25bbf839
Nima Dorjee lama <nimadorjee21@gmail.com>
2025-02-06 18:53:12 +0530

    update

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Make sure this file does NOT have 'use client'
+export default function Footer() {
+    return (
+      <nav>
+        <h1>My Footer</h1>
+      </nav>
+    );
+  }
